b856bba088af05aa39172f97326fbf6045226cdc
ginohmk <michotall95@gmail.com>
2022-07-24 14:21:08 +0100

    Add form collection merger

--- src/components/addProduct/formUtils/Ultils.js
+++ src/components/addProduct/formUtils/Ultils.js
@@ -9,19 +9,27 @@ const selectedFormItem = (state) => {
 };
 
 export const getFormValue = (selectedValue, e) => {
-  console.log(selectedValue);
+  const defaultFormValues = {
+    sku: e.target.sku.value,
+    name: e.target.name.value,
+    price: e.target.price.value,
+  };
+
   switch (selectedValue) {
     case 'DVD':
       return {
+        ...defaultFormValues,
         DVD: e.target.DVD.value,
       };
     case 'Book':
       return {
+        ...defaultFormValues,
         Book: e.target.book.value,
       };
 
     case 'Furniture':
       return {
+        ...defaultFormValues,
         height: e.target.height.value,
         weight: e.target.weight.value,
         length: e.target.length.value,
